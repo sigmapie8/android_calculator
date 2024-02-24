eb2720fbf5643433cea99d2a8b14427b71b847bc
sigmapie8 <garg.manav8@gmail.com>
2024-02-25 00:06:33 +0530

    ⚡️ migrated from infix_expression_parser to custom Infix class

--- lib/controller/calculatorController.dart
+++ lib/controller/calculatorController.dart
@@ -1,45 +1,31 @@
-import 'dart:developer';
+import 'dart:developer' as dev;
 
-import 'package:android_calculator/utility/enums.dart';
+import 'package:android_calculator/utility/infix.dart';
 import 'package:flutter/material.dart';
-import 'package:infix_expression_parser/expression_evaluator.dart';
-import 'package:infix_expression_parser/infix_expression_converter.dart';
 
 class CalculatorController extends ChangeNotifier {
   double? result;
   String expression = ""; // infix expression
 
-  String convertInfixToPostfix(String infixExpression) {
-    final converter = InfixExpressionConverter(expression: infixExpression);
-    return converter.toPostfixNotation();
-  }
-
-  double calculatePostfixValue(String postfixExpression) {
-    final result = ExpressionEvaluator.evaluatePostfix(
-        expression: postfixExpression, context: {});
-    return result as double;
-  }
-
   double? evaluateExpression(String expression) {
     try {
-      final postfixExpression = convertInfixToPostfix(expression);
-      return calculatePostfixValue(postfixExpression);
+      return double.parse(Infix.toValue(expression));
     } catch (error, st) {
-      log(error.toString());
-      log(st.toString());
+      dev.log(error.toString());
+      dev.log(st.toString());
       return null;
     }
   }
 
-  void onButtonClick(ButtonType buttonType, String value) {
-    if (buttonType == ButtonType.backspace && expression.isNotEmpty) {
-      expression = expression.substring(0, expression.length - 1);
-    } else if (buttonType == ButtonType.equals) {
-    } else {
-      expression += value;
-    }
-    result = evaluateExpression(expression);
+  // void onButtonClick(ButtonType buttonType, String value) {
+  //   if (buttonType == ButtonType.backspace && expression.isNotEmpty) {
+  //     expression = expression.substring(0, expression.length - 1);
+  //   } else if (buttonType == ButtonType.equals) {
+  //   } else {
+  //     expression += value;
+  //   }
+  //   result = evaluateExpression(expression);
 
-    notifyListeners();
-  }
+  //   notifyListeners();
+  // }
 }
