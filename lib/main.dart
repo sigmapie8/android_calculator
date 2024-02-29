2379515f2449800ce1b3b148eb1187b519337cb5
sigmapie8 <garg.manav8@gmail.com>
2024-03-01 01:27:58 +0530

    🚧 functional calculator with few glitches

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
+import 'package:android_calculator/calculator.dart';
 import 'package:android_calculator/controller/calculatorController.dart';
-import 'package:android_calculator/widgets/button_area.dart';
-import 'package:android_calculator/widgets/equation_area.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -13,46 +12,29 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MultiProvider(
-      providers: [
-        ChangeNotifierProvider(create: (_) => CalculatorController()),
-      ],
+    return ChangeNotifierProvider(
+      create: (context) => CalculatorController(),
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
         theme: ThemeData(
-          useMaterial3: false,
+            useMaterial3: true,
             primaryColor: const Color(0xFF27343C),
+            scaffoldBackgroundColor: const Color(0xFF181C1F),
             textTheme: const TextTheme(
-                displaySmall: TextStyle(
-              color: Color(0xFFD5E4EC),
-            ))),
-        home: const Sample(), // Sample MainPage Test1 ButtonScreen
-
-      ),
-    );
-  }
-}
-
-class Sample extends StatelessWidget {
-  const Sample({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.black,
-      body: SafeArea(
-        child: SingleChildScrollView(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              EquationArea(
-                calculatorController: CalculatorController(),
+              displaySmall: TextStyle(
+                color: Color(0xFFD5E4EC),
+                fontSize: 35,
               ),
-              const ButtonArea(isMinimized: false),
-            ],
-          ),
-        ),
+              displayMedium: TextStyle(
+                color: Colors.white,
+                fontSize: 40,
+              ),
+              displayLarge: TextStyle(
+                color: Colors.white,
+                fontSize: 60,
+              ),
+            )),
+        home: const Calculator(),
       ),
     );
   }
